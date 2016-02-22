24ee0782bc02d5c6e9cc9f8b6276c31130bf605f
Neil Gupta <neil@metamorphium.com>
2016-02-22 17:55:24 -0600

    Update slacker to 1.0.1

--- slacker.rb
+++ slacker.rb
@@ -1,8 +1,8 @@
 class Slacker < Formula
   desc "Simple command line utility for posting messages to Slack"
   homepage "https://github.com/neilgupta/slacker"
-  url "https://github.com/neilgupta/slacker/releases/download/1.0/slacker-1.0.tar.gz"
-  sha256 "ccc94db8fa7441f62e97b5223b1eab2316315309af716d70335c3e1c76e0847d"
+  url "https://github.com/neilgupta/slacker/releases/download/1.0.1/slacker-1.0.1.tar.gz"
+  sha256 "6f4d17ca27208b4126ca1aaf51a216a24a34a4b2cc0af5e76d2acee37bd007a1"
 
   bottle :unneeded
 
