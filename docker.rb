1c33ecf71db5a62b738af2e58768e9d672db057d
Neil Gupta <neil@metamorphium.com>
2016-02-22 19:47:12 -0600

    Update docker.rb

--- docker.rb
+++ docker.rb
@@ -2,6 +2,7 @@ class Docker < Formula
   desc "Utility to auto-login to docker.insops.net"
   homepage "https://docker.insops.net"
   url "gerrit:docker-insops", :branch => "refs/changes/66/72766/1", :using => :git
+  version '20160222'
 
   bottle :unneeded
 
