75166aeb8bbaef1a8c570bca39a61191d552f144
Neil Gupta <neil@metamorphium.com>
2016-02-22 19:42:03 -0600

    Add docker-insops formula

---
+++ docker-insops.rb
@@ -0,0 +1,12 @@
+class Docker-Insops < Formula
+  desc "Utility to auto-login to docker.insops.net"
+  homepage "https://docker.insops.net"
+  url "gerrit:docker-insops", :branch => "refs/changes/66/72766/1", :using => :git
+
+  bottle :unneeded
+
+  def install
+    system "gem install highline"
+    bin.install "docker-insops"
+  end
+end
