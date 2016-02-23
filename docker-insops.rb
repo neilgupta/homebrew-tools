2f6d3918f621b5327c0ee64732bf7e6893313ee2
Neil Gupta <neil@metamorphium.com>
2016-02-22 20:31:41 -0600

    Update docker-insops.rb

--- docker-insops.rb
+++ docker-insops.rb
@@ -1,13 +1,14 @@
 class DockerInsops < Formula
   desc "Utility to auto-login to docker.insops.net"
   homepage "https://docker.insops.net"
-  url "gerrit:docker-insops", :branch => "refs/changes/66/72766/1", :using => :git
+  url "gerrit:docker-insops", :using => :git
   version '20160222'
 
   bottle :unneeded
 
   def install
     system "gem install highline"
+    system "git fetch gerrit:docker-insops refs/changes/66/72766/1 && git checkout FETCH_HEAD"
     bin.install "docker-insops"
   end
 end
