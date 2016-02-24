8421a5cbd271a2e9131499aebb47c28bc1d0caa2
Neil Gupta <neil@metamorphium.com>
2016-02-24 12:24:26 -0600

    Create cg-deploy.rb

---
+++ cg-deploy.rb
@@ -0,0 +1,21 @@
+class CgDeploy < Formula
+  desc "Deploy script for Cloudgate"
+  homepage "https://gerrit.instructure.com/plugins/gitblit/blob/?f=README.md&r=cg-deploy.git&h=dev/1.0"
+  url "gerrit:cg-deploy", :branch => "dev/v1.0", :using => :git
+  version '1.0'
+
+  depends_on "docker-insops"
+  depends_on "slacker"
+  depends_on "codekitchen/dinghy/dinghy"
+
+  bottle :unneeded
+
+  def install
+    system "gem", "install", "aws-keychain-util", "--install-dir", prefix
+    bin.install "cg-deploy"
+  end
+
+  test do
+    system "#{bin}/cg-deploy", "version"
+  end
+end
